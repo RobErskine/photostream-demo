ab8f19391b468ac0f5254df5d64b82b3f50ca4fa
Rob Erskine <Rob@RobErskine.com>
2015-05-19 10:03:22 -0400

    adding in socket array pushes to photo uploads

--- build/js/main.js
+++ build/js/main.js
@@ -1,76 +1,38 @@
-$(function(){
+$(function() {
+    //$("form").ajaxForm(options);
+    $('form').on('submit',function(event){
+        event.preventDefault();
+        var post = new FormData($('form')[0]);
 
-    var options = { 
-        success: showResponse  // post-submit callback 
-    };
-
-    $('form').ajaxForm(options);
-
-    function showResponse(responseText, statusText, xhr, $form)  { 
-        console.log('status: ' + statusText + '\n\nresponseText: \n' + responseText + 
-            '\n\nThe output div should have already been updated with the responseText.'); 
-
-        //$form.parents('li').addClass('submitted');
-        $('.icon-camera').addClass('active');
-        $('.icon-spinner').removeClass('active');
-    }
+        $.ajax({
+            url:'/guestEntries/saveEntry',
+            type:'POST',
+            data: post,
+            contentType: false,
+            processData:false,
+            success: function(data, textStatus, jqXHR) {
+                //console.log(data);
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                //if fails      
+                console.log(textStatus);
+            }
+        });
+    });
 
     var upload = $('.upload-photo input[type="file"]');
 
-    $('button.upload').on('click',function(){
+    upload.on("change", function() {
+        $('.upload-photo-form input[type="submit"]').click();
+    });
+
+    $("button.upload").on("click", function() {
         upload.click();
-
-        upload.on('change',function(){
-            $('.upload-photo-form input[type="submit"]').click();
-        });
     });
 
-    $('.upload-photo-form').on('submit',function(event){
-        event.preventDefault();
-
-        $('.icon-camera').removeClass('active');
-        $('.icon-spinner').addClass('active');
+    $(".upload-photo-form").on("submit", function(event) {
+        event.preventDefault(); 
+        $(".icon-camera").removeClass("active");
+        $(".icon-spinner").addClass("active");
     });
-
-    /*/////////////////////////
-    // 60fps scroll paints
-    /////////////////////////*/
-    var body = $('body');
-    var timer;
-    $(window).on("scroll", function(){
-        if(! body.hasClass('disable-hover')){
-            body.addClass('disable-hover');
-        }
-        timer = setTimeout(function(){
-            body.removeClass('disable-hover');
-        }, 250);
-    }, false);
-
-
-    /*/////////////////////////
-    // Split up query strings
-    /////////////////////////*/
-    var getQueryVariable = function(variable){
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i=0;i<vars.length;i++) {
-            var pair = vars[i].split("=");
-            if(pair[0] === variable){
-                return pair[1];
-            }
-        }
-
-        return(false);
-    };
-
-    /*/////////////////////////
-    // open data-target in new tabs
-    /////////////////////////*/
-    var popupWindow = function(){
-        $("a[data-popup]").on("click", function(event){
-            event.preventDefault();
-            window.open($(this)[0].href);
-        });
-    };
-    popupWindow();
 });
--- html/js/main.js
+++ html/js/main.js
@@ -1,32 +1,25 @@
 $(function() {
-    function showResponse(responseText, statusText, xhr, $form) {
-        console.log("status: " + statusText + "\n\nresponseText: \n" + responseText + "\n\nThe output div should have already been updated with the responseText."),
-        $(".icon-camera").addClass("active"), $(".icon-spinner").removeClass("active");
-    }
-    var options = {
-        success: showResponse
-    };
-    $("form").ajaxForm(options);
+    $("form").on("submit", function(event) {
+        event.preventDefault();
+        var post = new FormData($("form")[0]);
+        $.ajax({
+            url: "/guestEntries/saveEntry",
+            type: "POST",
+            data: post,
+            contentType: !1,
+            processData: !1,
+            success: function(data, textStatus, jqXHR) {},
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
+            }
+        });
+    });
     var upload = $('.upload-photo input[type="file"]');
     upload.on("change", function() {
         $('.upload-photo-form input[type="submit"]').click();
-    });
-
-    $("button.upload").on("click", function() {
+    }), $("button.upload").on("click", function() {
         upload.click();
     }), $(".upload-photo-form").on("submit", function(event) {
         event.preventDefault(), $(".icon-camera").removeClass("active"), $(".icon-spinner").addClass("active");
     });
-    var timer, body = $("body");
-    $(window).on("scroll", function() {
-        body.hasClass("disable-hover") || body.addClass("disable-hover"), timer = setTimeout(function() {
-            body.removeClass("disable-hover");
-        }, 250);
-    }, !1);
-    var popupWindow = function() {
-        $("a[data-popup]").on("click", function(event) {
-            event.preventDefault(), window.open($(this)[0].href);
-        });
-    };
-    popupWindow();
 });
